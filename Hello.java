8c5a3c1e458a16622bd833e4eda8d418b73a0a94
fahlevi <91177922+xFahlevi@users.noreply.github.com>
2021-12-30 11:54:22 +0700

    Fifth Commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello{
     public static void  main(String[] args){
 	System.out.println("Tugas";
         System.out.println("xFahlevi: Aplikasi Java 2020");
-	System.out.println("blabla");
+	System.out.println("blublu");
 	System.out.println("END");
     }
 }
