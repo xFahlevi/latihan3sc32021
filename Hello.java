535720a8ccce3fd9d35e1aecc03f0c827fa07566
xFahlevi <fahlevizaa@gmail.com>
2021-12-30 11:44:59 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{
     public static void  main(String[] args){
+	System.out.println("Tugas";
         System.out.println("xFahlevi: Aplikasi Java 2020");
+	System.out.println("blabla");
     }
 }
