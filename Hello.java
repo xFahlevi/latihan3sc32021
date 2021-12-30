8533f687d2ed93491c17852678750036e9642f8a
xFahlevi <fahlevizaa@gmail.com>
2021-12-30 11:30:13 +0700

    Second Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
     public static void  main(String[] args){
-        System.out.println("xFahlevi: Aplikasi Java");
+        System.out.println("xFahlevi: Aplikasi Java 2020");
     }
 }
